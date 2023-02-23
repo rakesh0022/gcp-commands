d49521d783678131b95f947326b02314cfff6137
engineer <92107358+rakesh0022@users.noreply.github.com>
2023-02-24 00:41:18 +0530

    Update specialgcpcommand.groovy

--- specialgcpcommand.groovy
+++ specialgcpcommand.groovy
@@ -1,1 +1,41 @@
+pipeline {
+  agent any
+
+    stages {
+        stage('Plan') {
+      steps {
+        container('gcloud') {
+          sh "gcloud config set project ${GCP_PROJECT}"
+          sh "gcloud storage rm -r gs://${BUCKET_NAME} "
+        }
+      }
+        }
+        stage('Confirm') {
+      steps {
+        script {
+          def userInput = input(
+                        message: 'Do you want to proceed with the GCP command execution?',
+                        parameters: [booleanParam(defaultValue: false, description: 'Proceed with execution?')]
+                    )
+          if (!userInput) {
+            error('GCP command execution has been cancelled')
+          }
+        }
+      }
+        }
+        stage('Apply') {
+      steps {
+        container('gcloud') {
+          sh "gcloud config set project ${GCP_PROJECT}"
+          sh "gcloud storage rm -r gs://${BUCKET_NAME}"
+        }
+      }
+        }
+    }
+    post {
+        always {
+      cleanWs()
+        }
+    }
+}
 
