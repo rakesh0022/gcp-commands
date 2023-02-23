1f0afc30aa29723823db3a8de32c36be35832780
engineer <92107358+rakesh0022@users.noreply.github.com>
2023-02-24 00:46:05 +0530

    updated the code, now it has only one stage

--- specialgcpcommand.groovy
+++ specialgcpcommand.groovy
@@ -1,3 +1,4 @@
+/* groovylint-disable-next-line CompileStatic */
 pipeline {
   agent any
 
@@ -5,37 +6,10 @@ pipeline {
         stage('Plan') {
       steps {
         container('gcloud') {
-          sh "gcloud config set project ${GCP_PROJECT}"
-          sh "gcloud storage rm -r gs://${BUCKET_NAME} "
+          sh 'gcloud config set project qwiklabs-gcp-03-8348c6b63209'
+          sh 'gcloud version'
         }
       }
-        }
-        stage('Confirm') {
-      steps {
-        script {
-          def userInput = input(
-                        message: 'Do you want to proceed with the GCP command execution?',
-                        parameters: [booleanParam(defaultValue: false, description: 'Proceed with execution?')]
-                    )
-          if (!userInput) {
-            error('GCP command execution has been cancelled')
-          }
-        }
-      }
-        }
-        stage('Apply') {
-      steps {
-        container('gcloud') {
-          sh "gcloud config set project ${GCP_PROJECT}"
-          sh "gcloud storage rm -r gs://${BUCKET_NAME}"
-        }
-      }
-        }
     }
-    post {
-        always {
-      cleanWs()
-        }
-    }
+  }
 }
-
